df3c97c2bb8e91916a066b0d447b010f1dd4be1a
Tarek LAMMOUCHI <csj0oe@gmail.com>
2021-01-01 14:00:09 +0100

    generated voronoi

---
+++ Assets/VoronoiDemo.cs
@@ -0,0 +1,144 @@
+using UnityEngine;
+using System.Collections.Generic;
+using Delaunay;
+using Delaunay.Geo;
+
+public class VoronoiDemo : MonoBehaviour
+{
+
+    public Material land;
+    public const int NPOINTS = 200;
+    public const int WIDTH = 200;
+    public const int HEIGHT = 200;
+	public float freqx = 0.02f, freqy = 0.018f, offsetx = 0.43f, offsety = 0.22f;
+
+    private List<Vector2> m_points;
+	private List<LineSegment> m_edges = null;
+	private List<LineSegment> m_spanningTree;
+	private List<LineSegment> m_delaunayTriangulation;
+	private Texture2D tx;
+
+	private float [,] createMap() 
+    {
+        float [,] map = new float[WIDTH, HEIGHT];
+        for (int i = 0; i < WIDTH; i++)
+            for (int j = 0; j < HEIGHT; j++)
+                map[i, j] = Mathf.PerlinNoise(freqx * i + offsetx, freqy * j + offsety);
+        return map;
+    }
+
+	void Start ()
+	{
+        float [,] map=createMap();
+        Color[] pixels = createPixelMap(map);
+
+        /* Create random points points */
+		m_points = new List<Vector2> ();
+		List<uint> colors = new List<uint> ();
+		for (int i = 0; i < NPOINTS; i++) {
+			int x = Random.Range(0, WIDTH-1);
+			int y = Random.Range(0, HEIGHT-1);
+			int iter = 0;
+			while (iter < 10 && map[x,y] < 0.7) {
+				x = Random.Range(0, WIDTH-1);
+				y = Random.Range(0, HEIGHT-1);
+				iter++;
+			}
+			colors.Add ((uint)0);
+			Vector2 vec = new Vector2(x, y); 
+			m_points.Add (vec);
+		}
+
+		/* Generate Graphs */
+		Delaunay.Voronoi v = new Delaunay.Voronoi (m_points, colors, new Rect (0, 0, WIDTH, HEIGHT));
+		m_edges = v.VoronoiDiagram ();
+		m_spanningTree = v.SpanningTree (KruskalType.MINIMUM);
+		m_delaunayTriangulation = v.DelaunayTriangulation ();
+
+		/* Shows Voronoi diagram */
+		Color color = Color.blue;
+		for (int i = 0; i < m_edges.Count; i++) {
+			LineSegment seg = m_edges [i];				
+			Vector2 left = (Vector2)seg.p0;
+			Vector2 right = (Vector2)seg.p1;
+			DrawLine (pixels,left, right,color);
+		}
+
+		/* Shows Delaunay triangulation */
+		/*
+ 		color = Color.red;
+		if (m_delaunayTriangulation != null) {
+			for (int i = 0; i < m_delaunayTriangulation.Count; i++) {
+					LineSegment seg = m_delaunayTriangulation [i];				
+					Vector2 left = (Vector2)seg.p0;
+					Vector2 right = (Vector2)seg.p1;
+					DrawLine (pixels,left, right,color);
+			}
+		}*/
+
+		/* Shows spanning tree */
+		/*
+		color = Color.black;
+		if (m_spanningTree != null) {
+			for (int i = 0; i< m_spanningTree.Count; i++) {
+				LineSegment seg = m_spanningTree [i];				
+				Vector2 left = (Vector2)seg.p0;
+				Vector2 right = (Vector2)seg.p1;
+				DrawLine (pixels,left, right,color);
+			}
+		}*/
+
+		/* Apply pixels to texture */
+		tx = new Texture2D(WIDTH, HEIGHT);
+        land.SetTexture ("_MainTex", tx);
+		tx.SetPixels (pixels);
+		tx.Apply ();
+
+	}
+
+
+
+    /* Functions to create and draw on a pixel array */
+    private Color[] createPixelMap(float[,] map)
+    {
+        Color[] pixels = new Color[WIDTH * HEIGHT];
+        for (int i = 0; i < WIDTH; i++)
+            for (int j = 0; j < HEIGHT; j++)
+            {
+                pixels[i * HEIGHT + j] = Color.Lerp(Color.white, Color.black, map[i, j]);
+            }
+        return pixels;
+    }
+    private void DrawPoint (Color [] pixels, Vector2 p, Color c) {
+		if (p.x<WIDTH&&p.x>=0&&p.y<HEIGHT&&p.y>=0) 
+		    pixels[(int)p.x*HEIGHT+(int)p.y]=c;
+	}
+	// Bresenham line algorithm
+	private void DrawLine(Color [] pixels, Vector2 p0, Vector2 p1, Color c) {
+		int x0 = (int)p0.x;
+		int y0 = (int)p0.y;
+		int x1 = (int)p1.x;
+		int y1 = (int)p1.y;
+
+		int dx = Mathf.Abs(x1-x0);
+		int dy = Mathf.Abs(y1-y0);
+		int sx = x0 < x1 ? 1 : -1;
+		int sy = y0 < y1 ? 1 : -1;
+		int err = dx-dy;
+		while (true) {
+            if (x0>=0&&x0<WIDTH&&y0>=0&&y0<HEIGHT)
+    			pixels[x0*HEIGHT+y0]=c;
+
+			if (x0 == x1 && y0 == y1) break;
+			int e2 = 2*err;
+			if (e2 > -dy) {
+				err -= dy;
+				x0 += sx;
+			}
+			if (e2 < dx) {
+				err += dx;
+				y0 += sy;
+			}
+		}
+	}
+}
